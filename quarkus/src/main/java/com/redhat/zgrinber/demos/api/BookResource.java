97f9a89592372acccb4e571e4d078555dc3a2cc0
Zvi Grinberg <zgrinber@redhat.com>
2023-05-02 12:23:02 +0300

    correct quarkus configuration of  http-method-as-scope configuration to works with keycloak authorization server, and its permissions and scopes to protect a REST resource
    Signed-off-by: Zvi Grinberg <zgrinber@redhat.com>

--- quarkus/src/main/java/com/redhat/zgrinber/demos/api/BookResource.java
+++ quarkus/src/main/java/com/redhat/zgrinber/demos/api/BookResource.java
@@ -4,6 +4,7 @@ import com.redhat.zgrinber.demos.model.BookModel;
 import com.redhat.zgrinber.demos.services.BooksService;
 import io.quarkus.security.Authenticated;
 import io.quarkus.security.identity.SecurityIdentity;
+import jakarta.annotation.security.RolesAllowed;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import jakarta.inject.Named;
@@ -18,7 +19,7 @@ import java.util.List;
 
 @ApplicationScoped
 @Path("/api/books")
-
+@Authenticated
 public class BookResource {
     private static final Logger LOG = Logger.getLogger(BookResource.class);
     @Inject
@@ -28,17 +29,18 @@ public class BookResource {
     protected BooksService getBooksService;
 
     @GET
-    @Authenticated
+//    @RolesAllowed(value = "write_book")
     public List<BookModel> getAll() {
         return getBooksService.getAllBooks();
     }
     @GET
     @Path("/{id}")
+//    @RolesAllowed(value = {"write_book", "read_book"})
     public BookModel getOne(@PathParam("id") String id) {
         return getBooksService.getBook(id);
     }
-    @Authenticated
     @POST
+//    @RolesAllowed(value = "write_book")
     public Response CreateOne(@RequestBody @Valid BookModel book) {
             Response result;
             try
