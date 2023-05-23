25329091c91b702376ec4982441156ebac1ba80e
Zvi Grinberg <zgrinber@redhat.com>
2023-05-23 15:53:36 +0100

    Add KeyCloakResource Model for holding all resources and their permission scopes, and readiness response payload model for actuator readiness endpoint
    Signed-off-by: Zvi Grinberg <zgrinber@redhat.com>

--- springboot/src/main/java/com/redhat/zgrinber/keycloakintegrationspringboot/keycloak/KeycloakResource.java
+++ springboot/src/main/java/com/redhat/zgrinber/keycloakintegrationspringboot/keycloak/KeycloakResource.java
@@ -1,2 +1,18 @@
-package com.redhat.zgrinber.keycloakintegrationspringboot.keycloak;public class KeycloakResources {
+package com.redhat.zgrinber.keycloakintegrationspringboot.keycloak;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class KeycloakResource {
+  private String name;
+  private List<String> scopes;
+
 }
--- springboot/src/main/java/com/redhat/zgrinber/keycloakintegrationspringboot/model/ReadinessPayload.java
+++ springboot/src/main/java/com/redhat/zgrinber/keycloakintegrationspringboot/model/ReadinessPayload.java
@@ -1,2 +1,25 @@
-package com.redhat.zgrinber.keycloakintegrationspringboot.model;public class ReadinessPayload {
+package com.redhat.zgrinber.keycloakintegrationspringboot.model;
+
+import com.redhat.zgrinber.keycloakintegrationspringboot.keycloak.KeycloakResource;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ReadinessPayload {
+
+    private String status;
+    private Boolean allRolesExists;
+    private List roles;
+    private Boolean resourcesAndPermissionsExists;
+    private List<KeycloakResource> permissions;
+    private Boolean customClientScopesExist;
+    private List customClientScopes;
+
+
+
 }
